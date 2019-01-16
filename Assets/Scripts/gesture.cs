f3a38a4a43898defe120cc7c0c92f9b03872d28a
Keith <haw123@ucsd.edu>
2019-01-15 22:09:28 -0800

    upload files

---
+++ Assets/Scripts/gesture.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Leap;
+using UnityEngine;
+
+public class gesture : MonoBehaviour {
+    Hand lefthand;
+    Hand righthand;
+    Vector3 l_position;
+    Vector3 r_position;
+    Frame frame;
+    float speed;
+    Controller controller;
+    Vector3 midpoint0;
+    Vector3 wheel0;
+    bool triggered;
+    public static bool countstart;
+    public AudioClip engine;
+    public GameObject planemodel;
+    public GameObject planemodel2;
+    AudioSource source;
+    bool start;
+    int counter;
+    int switchcount;
+    // Use this for initialization
+    void Start () {
+        controller = new Controller();
+        switchcount = 100;
+        frame = controller.Frame();
+        speed = 0;
+        start = false;
+        triggered = false;
+        //triggering = false;
+        midpoint0 = Vector3.zero;
+        //lefthand = hands[0];
+        //righthand = hands[2];
+        counter = 30;
+        countstart = false;
+        source = GetComponent<AudioSource>();
+        source.volume = 0.3f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        controller = new Controller();
+        frame = controller.Frame();
+        List<Hand> hands = frame.Hands;
+        speed = 0;
+        if (hands.Count == 2) {
+            counter = 30;
+            lefthand = hands[0];
+            righthand = hands[1];
+            if (lefthand.IsRight)
+            {
+                righthand = hands[0];
+                lefthand = hands[1];
+            }
+            List<Finger> fingerlist1 = lefthand.Fingers;
+            List<Finger> fingerlist2 = righthand.Fingers;
+            int left_extend = 0;
+            int right_extend = 0;
+            for(int i = 0; i < fingerlist1.Count; i++)
+            {
+                if (fingerlist1[i].IsExtended)
+                {
+                    left_extend++;
+                }
+            }
+
+            for (int i = 0; i < fingerlist2.Count; i++)
+            {
+                if (fingerlist2[i].IsExtended)
+                {
+                    right_extend++;
+                }
+            }
+
+            if (left_extend == 0 && right_extend != 0)
+            {
+                //print("leftfist");
+                switchcount--;
+                if (switchcount <= 0)
+                {
+                    planemodel.active = false;
+                    planemodel2.active = false;
+                    switchcount = 100;
+                }
+            }
+            else if(left_extend != 0 && right_extend == 0)
+            {
+                //print("rightfist");
+                switchcount--;
+                if (switchcount <= 0)
+                {
+                    planemodel.active = true;
+                    planemodel2.active = false;
+                    switchcount = 100;
+                }
+            }
+            else if(left_extend == 0 && right_extend == 0)
+            {
+                //print("bothfist");
+                switchcount--;
+                if (switchcount <= 0)
+                {
+                    planemodel.active = false;
+                    planemodel2.active = true;
+                    switchcount = 100;
+                }
+            }
+            Vector diff = lefthand.PalmPosition - righthand.PalmPosition;
+            //print(diff.Magnitude);
+            Vector midpointv = lefthand.PalmPosition - 0.5f * diff;
+            if (diff.Magnitude <= 150) { 
+                start = true;
+                countstart = true;
+            }
+            if (diff.Magnitude > 150 && start &&trackloader.gamestart)
+            {
+                
+                speed = (diff.Magnitude - 150.0f) / 100.0f;
+                if (!triggered)
+                {
+                    triggered = true;
+                    midpoint0 = new Vector3(midpointv.x, midpointv.y, midpointv.z);
+                    midpoint0.x = 0;
+                    midpoint0 = Vector3.Normalize(midpoint0);
+                    wheel0 = new Vector3(diff.x, diff.y, diff.z);
+                    wheel0.z = 0;
+                    wheel0 = Vector3.Normalize(wheel0);
+                }
+                Vector3 midpoint =new Vector3(midpointv.x, midpointv.y, midpointv.z);
+                midpoint.x = 0;
+                midpoint = Vector3.Normalize(midpoint);
+                Vector3 wheel = new Vector3(diff.x, diff.y, diff.z);
+                wheel.z = 0;
+                wheel = Vector3.Normalize(wheel);
+                if (triggered)
+                {
+                    float angle = (Vector3.SignedAngle(midpoint0, midpoint, Vector3.right)) / 20.0f;
+                    transform.parent.parent.parent.Rotate(new Vector3(angle, 0, 0));
+                    float angle2 = (Vector3.SignedAngle(wheel0, wheel, Vector3.back)) / 20.0f;
+                    transform.parent.parent.parent.Rotate(new Vector3(0, angle2, 0));
+                }
+            }
+            else
+            {
+                triggered = false;
+            }
+            
+            //Vector3 ydirection = Vector3.Normalize(midpoint - transform.parent.parent.parent.position);
+            //float angle = Vector3.Angle(midpoint, transform.parent.parent.parent.forward);
+            //transform.parent.parent.parent.rotation *= angle;
+        }
+        else
+        {
+            start = false;
+        }
+        if (speed != 0 && hands.Count == 2 && trackloader.gamestart)
+        {
+            source.pitch = speed * 0.5f;
+            source.PlayOneShot(engine);
+        }
+        else
+        {
+            source.Stop();
+        }
+        transform.parent.parent.parent.position += transform.parent.parent.parent.forward * speed;
+        
+    }
+}
---
+++ Assets/Scripts/trackloader.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+public class trackloader : MonoBehaviour {
+    string trackdir = "Assets/Tracks/track2.txt";
+    public GameObject checkpoint;
+    GameObject curr_point;
+    LineRenderer line;
+    LineRenderer line2;
+    GameObject waypoint;
+    List<GameObject> waypoints;
+    public AudioClip crush;
+    public AudioClip beep1;
+    public AudioClip beep2;
+    public AudioClip beep3;
+    public AudioClip fin;
+    public AudioClip sonar;
+    AudioSource source;
+    AudioSource radar;
+    public Material mat;
+    public Material mat2;
+    public GameObject navigator;
+    public GameObject player;
+    public GameObject Arrow;
+    public Text text;//counter, go, finish
+    public Text text2;//time in second;
+    public Text text3;//lap 
+    public Text text4;//distance
+    int index;
+    public static bool gamestart;
+    bool gameover;
+    bool timerstart;
+    bool startsound;
+    float countdown = 3;
+    int i_countdown = 3;
+    int radar_counter = 100;
+    float timer;
+    Quaternion a_rotate;
+	// Use this for initialization
+	void Start () {
+        index = 1;
+        timer = 0;
+        waypoints = new List<GameObject>();
+        StreamReader reader = new StreamReader(trackdir);
+        source = GetComponent<AudioSource>();
+        string temp;
+        string[] xyz_s;
+        Vector3 xyz;
+        while ((temp = reader.ReadLine()) != null)
+        {
+            xyz_s = temp.Split(' ');
+            xyz.x = float.Parse(xyz_s[0]) / 39.37f;
+            xyz.y = float.Parse(xyz_s[1]) / 39.37f;
+            xyz.z = float.Parse(xyz_s[2]) / 39.37f;
+            waypoint = Instantiate(checkpoint, xyz, checkpoint.transform.rotation);
+            waypoints.Add(waypoint);
+        }
+        curr_point = waypoints[index];
+        player.transform.position = waypoints[0].transform.position;
+        Vector3 pos = waypoints[1].transform.position;
+        pos.y = 0;
+        player.transform.LookAt(pos);
+        line = gameObject.AddComponent<LineRenderer>();
+        line.startWidth = 0.5f;
+        line.endWidth = 0.5f;
+        line.positionCount = waypoints.Count;
+        line2 = navigator.AddComponent<LineRenderer>();
+        line2.startWidth = 0.1f;
+        line2.endWidth = 0.1f;
+        line2.positionCount = 2;
+        line.material = mat;
+        line2.material = mat2;
+        for (int i = 0; i < line.positionCount; i++)
+        {
+            line.SetPosition(i, waypoints[i].transform.position);
+        }
+        gamestart = false;
+        gameover = false;
+        timerstart = false;
+        startsound = false;
+        text.text = "Ready?";
+        text3.text = "0/" + (waypoints.Count-1).ToString();
+        a_rotate = Arrow.transform.rotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (line2 != null)
+        {
+            line2.SetPosition(0, navigator.transform.position);
+            line2.SetPosition(1, curr_point.transform.position);
+            Arrow.transform.LookAt(curr_point.transform.position);
+            Arrow.transform.rotation = a_rotate * Arrow.transform.rotation;
+            if (gamestart)
+            {
+                text4.text = Vector3.Distance(navigator.transform.position, curr_point.transform.position).ToString("0.00") + "m";
+            }
+            else
+            {
+                text4.text = "";
+            }
+        }
+        else
+        {
+            text4.text = "";
+        }
+
+        if(countdown >= -1 && gesture.countstart)
+        {
+            countdown -= Time.deltaTime;
+            if (!startsound)
+            {
+                startsound = true;
+                text.text = "3";
+                source.PlayOneShot(beep1);
+            }
+        }
+        if(countdown <= i_countdown - 1 && i_countdown > 0)
+        {
+            i_countdown -= 1;
+            text.text = i_countdown.ToString();
+            if (i_countdown >= 1)
+            {
+                source.PlayOneShot(beep1);
+            }
+            else
+            {
+                source.PlayOneShot(beep2);
+            }
+        }
+        if(i_countdown <= 0)
+        {
+            text.text = "";
+            gamestart = true;
+            timerstart = true;
+        }
+        if (timerstart && !gameover)
+        {
+            timer += Time.deltaTime;
+            text2.text = timer.ToString("0.00") + "s";
+        }
+        if(countdown <= -1)
+        {
+            if (gameover)
+            {
+                text.text = "Fin!";
+            }
+            else
+            {
+                text.text = "";
+            }
+        }
+        text3.text = (index-1).ToString() + "/" + (waypoints.Count - 1).ToString();
+        /*
+        radar_counter--;
+        if (radar_counter <= 0)
+        {
+            radar = curr_point.GetComponent<AudioSource>();
+            radar.volume = 1.0f;
+            radar.loop = true;
+            if (!radar.isPlaying)
+                radar.PlayOneShot(sonar);
+            radar_counter = 100;
+        }
+        */
+    }
+
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "check")
+        {
+            if(collision.gameObject == curr_point)
+            {
+                index ++;
+                if (index < waypoints.Count - 1)
+                {
+                    curr_point = waypoints[index];
+                }
+                print(index);
+            }
+        }
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "check")
+        {
+            if (other.gameObject == curr_point)
+            {
+                index++;
+                if (index < waypoints.Count)
+                {
+   
+                    curr_point = waypoints[index];
+                    print(index);
+                    source.PlayOneShot(beep3);
+                }
+                else
+                {
+                    Destroy(line2);
+                    gameover = true;
+                    source.PlayOneShot(fin);
+                }
+                
+            }
+        }
+        else
+        {
+            if (!gameover) {
+                source.PlayOneShot(crush);
+                player.transform.position = waypoints[index - 1].transform.position;
+                gamestart = false;
+                text.text = "3";
+                countdown = 3;
+                i_countdown = 3;
+                Vector3 pos = waypoints[index].transform.position;
+                pos.y = 0;
+                player.transform.LookAt(pos);
+                //print("crush");
+            }
+        }
+    }
+}
